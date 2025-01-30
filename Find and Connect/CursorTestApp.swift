0120195cc83cf3e1905c1aff47bde081c7810987
billyh2 <billyh2@illinois.edu>
2025-01-30 14:01:13 -0600

    added current code

---
+++ Find and Connect/CursorTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursorTestApp.swift
+//  CursorTest
+//
+//  Created by Billy Huang on 2025/1/29.
+//
+
+import SwiftUI
+
+@main
+struct CursorTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
