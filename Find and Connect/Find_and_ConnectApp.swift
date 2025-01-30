e6311b67d01cb69618c2e12247dbf9112f55c16e
billyh2 <billyh2@illinois.edu>
2025-01-30 14:11:58 -0600

    Initial Commit

---
+++ Find and Connect/Find_and_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_and_ConnectApp.swift
+//  Find and Connect
+//
+//  Created by Billy Huang on 2025/1/30.
+//
+
+import SwiftUI
+
+@main
+struct Find_and_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
